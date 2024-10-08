336b7c31a0af98bb4bdaa53d89e1d2b4a3617eda
afevfev <fivamuluken@gmail.com>
2024-10-08 06:46:52 +0300

    add search functionality using  API

--- src/js.js
+++ src/js.js
@@ -1,8 +1,26 @@
-function CityWeather(event){
-  event.preventDefault();
-  let input = document.querySelector(".input"); 
-  let city = document.querySelector(".city");
-  city.innerHTML = input.value;
+function refreshWeather(response) {
+  let temp = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector(".city");
+
+  cityElement.innerHTML = response.data.city;
+  temp.innerHTML = Math.round(temperature);
 }
-let selector = document.querySelector(".search");
-selector.addEventListener("submit",CityWeather)
+
+function searchCity(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let Input = document.querySelector(".input");
+
+  searchCity(Input.value);
+}
+
+let searchElement = document.querySelector(".search");
+searchElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
